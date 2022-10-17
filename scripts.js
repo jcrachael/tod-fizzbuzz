ffb6d88f0572dc2a720bfc0dc0d8d9088ee1891d
Rachael Cole <jcrachael@gmail.com>
2022-10-17 15:51:35 +1100

    css styling

--- scripts.js
+++ scripts.js
@@ -10,15 +10,19 @@ document.addEventListener("DOMContentLoaded", function(event) {
     for (let i = 1; i <= answer; i++) {
         // check if current number is divisible by 3 AND 5 and if yes print "FizzBuzz"
         if (i % 3 === 0 && i % 5 === 0) {
-            container.innerHTML += "FizzBuzz!\n\n";
+            container.innerHTML += "<p class='fizzbuzz'>FizzBuzz!</p>";
         // check if current number is divisible by 3 and if yes print "Fizz"
         } else if (i % 3 === 0) {
-            container.innerHTML += "Fizz"+ "\n\n";
+            container.innerHTML += "<p class='fizz'>Fizz!</p> ";
         // else if current number is divisible by 5 then print "Buzz"
         } else if (i % 5 === 0) {
-            container.innerHTML += "Buzz" + "\n\n";
+            container.innerHTML += "<p class='buzz'>Buzz!</p> ";
         } else {
-            container.innerHTML += i + "\n\n";
+            container.innerHTML += i + "..." + "\n\n";
         }
     }
+
+    container.innerHTML += "<p>You're all FizzBuzzed out!</p>\n\n"
+   
 });
+
